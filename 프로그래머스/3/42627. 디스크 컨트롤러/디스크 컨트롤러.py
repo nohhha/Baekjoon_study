6d74459a2f4db6e380f1b20aedfeb9b4882511b7
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-02 18:16:14 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.19 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,21 +1,31 @@
-import heapq
+import heapq as hq
 
 def solution(jobs):
-    answer,now,i=0,0,0
-    start=-1
-    heap=[]
+    answer = 0
+    # 우선순위: 작업의 소요시간 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것
+    # jobs: [작업이 요청되는 시점, 작업의 소요시간]
+    N = len(jobs)
+    ready = []
+    now = 0
+    complete = 0
     
-    while i<len(jobs):
-        for j in jobs:
-            if start<j[0]<=now:
-                heapq.heappush(heap, (j[1], j[0]))
-        if len(heap)>0:
-            current = heapq.heappop(heap)
-            start=now
-            now+=current[0]
-            answer+=(now-current[1])
-            i+=1
+    while complete<N:
+        
+        for i in range(len(jobs)-1, -1, -1):
+            job = jobs[i]
+            # 요청시각이 현재보다 이전인 것들 ready 에 추가
+            if job[0]<=now:
+                # (소요시간, 요청시각)
+                hq.heappush(ready, (job[1],job[0]))
+                jobs.pop(i)
+        
+        # 대기 중인 작업 있으면
+        if len(ready)>0:
+            duration, start = hq.heappop(ready)
+            now += duration
+            complete += 1
+            answer += (now-start)
         else:
-            now+=1
-
-    return int(answer/len(jobs))
+            now += 1
+    
+    return answer//N
