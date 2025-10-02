b74223dd30fb1ba38f973087e5e3e0ae438a6d4a
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-02 14:14:20 +0900

    [level 2] Title: 기능개발, Time: 0.58 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,24 +1,17 @@
-from collections import deque
-
 def solution(progresses, speeds):
-    answer = []
+    answer = []  
     
-    N = len(progresses)
-    days = [0]*N
-    for i in range(N):
-        day = 0
-        now = progresses[i]
-        while now<100:
-            now += speeds[i]
-            day += 1
-        days[i] = day
-    
-    for today in range(max(days)+1):
+    while progresses:
         cnt = 0
-        while days and days[0]<=today:
-            days.pop(0)
+        while progresses and progresses[0]>=100:
+            progresses.pop(0)
+            speeds.pop(0)
             cnt += 1
-        if cnt>0:
+            
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+            
+        if cnt!=0:
             answer.append(cnt)
     
     return answer
