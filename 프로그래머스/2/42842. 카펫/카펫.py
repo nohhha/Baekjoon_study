66d509633ffc82a3e7e2298af1bf7afc13cfa6c1
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-03 15:24:00 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,15 @@
 def solution(brown, yellow):
     answer = []
-    for y in range(1, int(yellow**0.5)+1):
-        if yellow%y==0:
-            if (y+yellow//y+2)*2==brown:
-                answer=[yellow//y+2, y+2]
-                break
+    
+    h = 1
+    while True:
+        if yellow%h == 0:
+            w = yellow//h
+            if ((w+2)*(h+2)) == (yellow+brown):
+                return [w+2, h+2]
+            else:
+                h += 1
+        else:
+            h += 1
+    
     return answer
