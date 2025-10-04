4e56ac0f5c009e835500d567ff1a11907333bb9a
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-04 15:13:25 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,20 @@
+
+
+def add_cnt(alpha, i):
+    dic = {"A":0, "E":1, "I":2, "O":3, "U":4}  
+    cnt  = 1
+    for j in range(1, 6-i):
+        cnt += 5**j
+    cnt *= dic[alpha]
+    return cnt+1
+
+def solution(word):
+    answer = 0
+    # "A"부터 "E"사이에는 1 + 5**1 + 5**2 + 5**3 + 5**4
+    # "AA"부터 "AE" 사이에는 1 + 5**1 + 5**2 + 5**3
+    
+    # 알파벳과 자릿수를 받으면 ("I", "1") 시작 위치를 알려주기 =>  2*(5**1 + 5**2 + 5**3 + 5**4)
+    for i,alpha in enumerate(word):
+        answer += add_cnt(alpha, i+1)
+        
+    return answer
