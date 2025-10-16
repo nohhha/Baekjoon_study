d08cde0ae89e15e6a776af43d6d9465c5e1b75f0
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-16 19:32:54 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,14 @@
+N = int(input())
+dp = [0]*(N+1)
+
+# 초기화
+dp[1] = 1
+if N>=2:
+    dp[2] = 2
+
+# 점화식
+if N>=3:
+    for i in range(3,N+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007
+
+print(dp[N])
