602fc0cc1af6318f35983982f7ad69c66d14351c
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-16 19:21:52 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -2,23 +2,18 @@ T = int(input())
 
 for _ in range(T):
     N = int(input())
-    dp1, dp2, dp3 = [0]*(N+1), [0]*(N+1), [0]*(N+1)
+    dp = [0]*(N+1)
     
     # 초기화
-    dp1[1] = 1
+    dp[1] = 1
     if N>=2:
-        dp1[2], dp2[2] = dp1[1], 1
+        dp[2] = 2
     if N>=3:
-        dp1[3] = dp1[2] + dp2[2]
-        dp2[3] = dp2[2]
-        dp3[3] = 1
+        dp[3] = 4
 
     # 점화식
-    if N>=3:
-        for i in range(4,N+1):
-            dp1[i] = dp1[i-1] + dp2[i-1] + dp3[i-1]
-            dp2[i] = dp1[i-2] + dp2[i-2] + dp3[i-2]
-            dp3[i] = dp1[i-3] + dp2[i-3] + dp3[i-3]
+    if N>=4:
+        for i in range(4, N+1):
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
 
-    answer = dp1[N] + dp2[N] + dp3[N]
-    print(answer)
+    print(dp[N])
