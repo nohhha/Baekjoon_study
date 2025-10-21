db19728f125bd597b80ed4fe81cb4c1db62f4105
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-21 11:06:55 +0900

    [Gold V] Title: 전깃줄, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -4,25 +4,12 @@ for _ in range(N):
     arr.append(list(map(int, input().split())))
 arr.sort(key=lambda x: x[0])
 
-# dp[i][j]: i번째 전깃줄 고려할 때, 전봇대 A 위치 j에서 가는 전깃줄 B의 위치
-dp = [[0]*501 for _ in range(N)]
-ends = [0]*501
-for s,e in arr:
-    ends[s] = e
+# dp[i] = 최대증가수열
+dp = [1]*N
 
-for i in range(N):
-    s,e = arr[i]
-    temp = []
-    for j in range(1,s+1):
-        if j<s:
-            dp[i][j] = dp[i-1][j]
-            if ends[j]!=0 and ends[j] < e:
-                temp.append(j)
-        else:
-            max_v = 0
-            if temp:
-                for k in temp:
-                    max_v = max(max_v, dp[i-1][k])
-            dp[i][j] = max(dp[i][j], max_v + 1)
+for i in range(1,N):
+    for j in range(0,i):
+        if arr[j][1] < arr[i][1]:
+            dp[i] = max(dp[i], dp[j]+1)
 
-print(N-max(dp[-1]))
+print(N-max(dp))
