d69192e0f4e97fe5ea3b6cb8b2d9132b6d87d296
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-29 15:59:12 +0900

    [Gold V] Title: 토마토, Time: 2700 ms, Memory: 100560 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -3,19 +3,15 @@ from collections import deque
 M,N = map(int, input().split())
 arr = []
 q = deque()
-v = [[0]*M for _ in range(N)]
 total = 0
 
 for i in range(N):
     arr.append(list(map(int, input().split())))
     for j in range(M):    
-        if arr[i][j]==-1:
-            v[i][j]=-1
-        else:
+        if arr[i][j]!=-1:
             total += 1
             if arr[i][j]==1:
                 q.append((i,j))
-                v[i][j]=1
 
 dxs = [-1,1,0,0]
 dys = [0,0,-1,1]    
@@ -26,10 +22,10 @@ while q:
     ci,cj = q.popleft()
     for di,dj in zip(dxs, dys):
         ni,nj = ci+di,cj+dj
-        if 0<=ni<N and 0<=nj<M and arr[ni][nj]==0 and v[ni][nj]==0:
+        if 0<=ni<N and 0<=nj<M and arr[ni][nj]==0:
             q.append((ni,nj))
-            v[ni][nj] = v[ci][cj]+1
-            max_day = v[ni][nj]
+            arr[ni][nj] = arr[ci][cj]+1
+            max_day = arr[ni][nj]
             cnt += 1
 
 if cnt<total:
