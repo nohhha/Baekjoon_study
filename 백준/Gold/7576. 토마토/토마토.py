3a20aa6fedbd3b054228a291a5ea1cb741e9c9a4
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-29 15:53:35 +0900

    [Gold V] Title: 토마토, Time: 2852 ms, Memory: 108444 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -4,20 +4,24 @@ M,N = map(int, input().split())
 arr = []
 q = deque()
 v = [[0]*M for _ in range(N)]
+total = 0
 
 for i in range(N):
     arr.append(list(map(int, input().split())))
-    for j in range(M):
-        if arr[i][j]==1:
-            q.append((i,j))
-            v[i][j]=1
-        elif arr[i][j]==-1:
+    for j in range(M):    
+        if arr[i][j]==-1:
             v[i][j]=-1
+        else:
+            total += 1
+            if arr[i][j]==1:
+                q.append((i,j))
+                v[i][j]=1
 
 dxs = [-1,1,0,0]
 dys = [0,0,-1,1]    
 
 max_day = 0
+cnt = len(q)
 while q:
     ci,cj = q.popleft()
     for di,dj in zip(dxs, dys):
@@ -26,17 +30,9 @@ while q:
             q.append((ni,nj))
             v[ni][nj] = v[ci][cj]+1
             max_day = v[ni][nj]
+            cnt += 1
 
-flag = True
-for i in range(N):
-    for j in range(M):
-        if v[i][j]==0:
-            flag=False
-            break
-    if not flag:
-        break
-
-if not flag:
+if cnt<total:
     print(-1)
 else:
     print(max(0, max_day-1))
