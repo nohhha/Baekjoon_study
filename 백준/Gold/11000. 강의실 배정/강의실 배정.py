f3157b08be63a7258aec3f5f4cb590a7c8ceb613
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-10-31 08:50:22 +0900

    [Gold IV] Title: 강의실 배정, Time: 324 ms, Memory: 75768 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+arr.sort()
+
+room = []
+heapq.heappush(room, arr[0][1])
+
+for i in range(1,N):
+    if arr[i][0] < room[0]:
+        heapq.heappush(room, arr[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, arr[i][1])
+print(len(room))
