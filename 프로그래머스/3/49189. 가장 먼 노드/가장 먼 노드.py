e692a832aa12617208a12621b3e1bc64ff3fdaf4
Hayeon Noh <95074063+nohhha@users.noreply.github.com>
2025-11-15 21:12:24 +0900

    [level 3] Title: 가장 먼 노드, Time: 46.94 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,24 +1,33 @@
 from collections import deque
+INF = 1e9
 
 def solution(n, edge):
     answer = 0
-    graph=[[] for _ in range(n+1)]
-    distance = [-1]*(n+1)
+    graph = [[] for _ in range(n+1)]
+    for a,b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
     
-    for e in edge:
-        graph[e[0]].append(e[1])
-        graph[e[1]].append(e[0])
+    start = 1
+    
+    dist = [INF]*(n+1)
+    q = deque()
+    dist[start] = 0
+    q.append(start)
+    
+    max_v = 0
+    answer = 0
+    while q:
+        ci = q.popleft()
+        for ni in graph[ci]:
+            if dist[ni]!=INF:
+                continue
+            dist[ni] = dist[ci]+1
+            q.append(ni)
+            if dist[ni]==max_v:
+                answer += 1
+            elif dist[ni]>max_v:
+                answer = 1
+                max_v = dist[ni]
         
-    queue = deque([1])
-    distance[1]=0
-    
-    while queue:
-        now = queue.popleft() 
-        for i in graph[now]:
-            if distance[i]==-1: #아직 방문하지 않은 노드인 경우
-                queue.append(i) #queue에 추가
-                distance[i] = distance[now]+1 #거리 추가
-    for d in distance:
-        if d==max(distance):
-            answer+=1
     return answer
